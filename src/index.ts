5eded176e4c79460c6f17b1b0fc78b98fd0976d4
Denis Eneotescu <deniseneotescu@gmail.com>
2024-10-10 22:00:00 +0300

    set up mikroORM and migrations

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroOrmConfig from './mikro-orm.config';
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+
+  await orm.getMigrator().up();
+
+  const em = orm.em.fork();
+  //   const post = em.create(Post, {
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //     title: 'my first post',
+  //   });
+  //   await em.persistAndFlush(post);
+
+  const posts = await em.find(Post, {});
+  console.log(posts);
+};
+
+main().catch((err) => console.error(err));
